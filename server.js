7d2f36d5a93bb9787c9be798f54e88b62750571f
Shaun Sweet <shaun@shaunsweet.com>
2017-07-14 16:28:46 -0700

    cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://alkasm.github.io");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
